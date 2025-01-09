7dfff9773ab95a4fdb1dd1809fdbe75bd9bc74bc
Charbel-5 <charbelmaroun23@gmail.com>
2025-01-09 18:04:55 +0200

    Adjusted Navbar Style

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,19 +1,23 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './MainNavbar.css';
 
 function MainNavbar() {
+  const location = useLocation();
+  const isDropdownActive = ['/settings', '/debt-calculator', '/stocks'].includes(location.pathname);
+
   return (
     <div className="topnav">
-      <Link to="/transactions" className="active">Transactions</Link>
-      <Link to="/accounts">Accounts</Link>
-      <Link to="/stats">Stats</Link>
+      <Link to="/transactions" className={location.pathname === '/transactions' ? 'active' : ''}>Transactions</Link>
+      <Link to="/accounts" className={location.pathname === '/accounts' ? 'active' : ''}>Accounts</Link>
+      <Link to="/stats" className={location.pathname === '/stats' ? 'active' : ''}>Stats</Link>
 
       <div className="dropdown">
-        <button className="dropbtn">More</button>
+        <a className={`dropbtn ${isDropdownActive ? 'active' : ''}`}>More</a>
         <div className="dropdown-content">
-          <Link to="/settings">Settings</Link>
-          <Link to="/debt-calculator">Debt Calculator</Link>
-          <Link to="/stocks">Stocks</Link>
+          <Link to="/settings" className={location.pathname === '/settings' ? 'active' : ''}>Settings</Link>
+          <Link to="/debt-calculator" className={location.pathname === '/debt-calculator' ? 'active' : ''}>Debt Calculator</Link>
+          <Link to="/stocks" className={location.pathname === '/stocks' ? 'active' : ''}>Stocks</Link>
         </div>
       </div>
 
