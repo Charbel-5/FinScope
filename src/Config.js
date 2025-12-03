db05d5658064c6b80df3d40d50067300b3367498
Charbel-5 <charbelmaroun23@gmail.com>
2025-12-03 14:04:14 +0200

    Deploy backend to EB, frontend on S3

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,4 @@
 const config = {
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
   polygonApiKey: process.env.REACT_APP_POLYGON_API_KEY || 'DnGGuI3gDXto9JPBvAVKuu8tHr15fWqG',
-};
-
-export default config;
+};export default config;
--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "finscope-db.cdewcsgsgqz8.me-central-1.rds.amazonaws.com",
+  user: "finscopeapp",
+  password: "StrongAppPass123!",
+  database: "finscope",
   waitForConnections: true,
   connectionLimit: 10,
 });
